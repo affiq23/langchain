7d8df826b49cb705eca107452384f64968c20a55
affiq23 <affiqem@gmail.com>
2024-07-08 17:55:38 -0500

    added generative AI functionality

--- tchat/main.py
+++ tchat/main.py
@@ -1,5 +1,27 @@
+from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import LLMChain
+from dotenv import load_dotenv
+import warnings
 
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+load_dotenv()
+chat = ChatOpenAI()
+
+prompt = ChatPromptTemplate(
+    input_variables=["content"],
+    messages=[
+        HumanMessagePromptTemplate.from_template("{content}")
+    ]
+)
+
+chain = LLMChain(
+    llm=chat,
+    prompt=prompt
+)
 
 while True:
     content = input(">> ")
-    print(f"You entered: {content}")
+    result = chain({"content": content})
+    print(result["text"])
