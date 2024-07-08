999da86b9a25879ab77a7e053934119f54a92bb8
affiq23 <affiqem@gmail.com>
2024-07-08 16:57:56 -0500

    chatbot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from langchain_openai import OpenAI
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
+from langchain.chains import LLMChain, SequentialChain
 from dotenv import load_dotenv
 import argparse
 import warnings
@@ -15,24 +15,43 @@ args = parser.parse_args()
 # ignore deprecation warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-llm = OpenAI(
-    
-)
+llm = OpenAI()
 
 code_prompt = PromptTemplate(
     template= "Write a very short {language} function that {task}",
     input_variables=["language", "task"]
 )
 
-code_chain = LLMChain(
-    llm=llm, 
-    prompt= code_prompt
+test_prompt = PromptTemplate(
+    template= "Write a test function for the following {language} code:/{code}",
+    input_variables=["language", "code"]
+
 )
 
-result = code_chain({
+code_chain = LLMChain(
+    llm=llm, 
+    prompt= code_prompt,
+    output_key="code"
+)
+
+test_chain = LLMChain(
+    llm=llm, 
+    prompt= test_prompt,
+    output_key="test"
+)
+
+chain = SequentialChain(
+    chains=[code_chain, test_chain],
+    input_variables=["language", "task"],
+    output_variables=["code", "test"]
+)
+
+result = chain({
     "language": args.language,
     "task": args.task
 })
-
-print(result['text'])
+print("GENERATED CODE")
+print(result["code"])
+print("GENERATED TEST")
+print(result["test"])
 
