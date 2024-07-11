d7add8b9c6a1e7e6fc4d7eb865e9f2b7ef41c9a1
affiq23 <affiqem@gmail.com>
2024-07-11 13:48:02 -0500

    added in memory buffer

--- tchat/main.py
+++ tchat/main.py
@@ -1,26 +1,47 @@
-from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate
+# import for prompt template
+from langchain.prompts import MessagesPlaceholder, HumanMessagePromptTemplate, ChatPromptTemplate
+# import for interacting with chat model
 from langchain.chat_models import ChatOpenAI
+# import for operations involving language models
 from langchain.chains import LLMChain
+# import for loading environment variables
 from dotenv import load_dotenv
+# import for ignoring deprecation warnings
 import warnings
+from langchain.memory import ConversationBufferMemory
+
 
 warnings.filterwarnings("ignore", category=DeprecationWarning)
-
+# loads env variables from .env file
 load_dotenv()
+# creates instance of OpeanAI GPT model
 chat = ChatOpenAI()
 
+# memory_key is key used to store any additional input variables; return_messages = True means return objects that wrap up the messages
+memory = ConversationBufferMemory(memory_key="messages", return_messages=True)
+
+# creates ChatPromptTemplate instance; defines how input will be formatted before being passed to the model
 prompt = ChatPromptTemplate(
-    input_variables=["content"],
+    # template expects input var called content to be passed
+    input_variables=["content", "messages"],
+    # generates a prompt from the input variable
     messages=[
+        # go look at input variables called messages
+        MessagesPlaceholder(variable_name="messages"),
         HumanMessagePromptTemplate.from_template("{content}")
     ]
 )
 
+# creates instance of LLMChain; responsible for processing input through language model
 chain = LLMChain(
-    llm=chat,
-    prompt=prompt
+    # responsible for processing input
+    llm = chat,
+    # responsible for formatting input
+    prompt = prompt,
+    memory = memory
 )
 
+# loop to keep the chat running
 while True:
     content = input(">> ")
     result = chain({"content": content})
